6221a654b371ee737a3c4c0cfa0c796b2c9dbddc
sma299 <59669463+sma299@users.noreply.github.com>
2020-01-30 19:26:41 -0500

    created a loop that looks like a receipt at the end

--- shopping_cart.py
+++ shopping_cart.py
@@ -34,23 +34,30 @@ products = [
 # INFO CAPTURE (INPUT)
 #
 total_price = 0
+selected_ids = [] #create a list of ids selected
 
 while True:
 
-    selected_id = input("Please input a product identifier: ") # this is a string
+    selected_id = input("Please input a product identifier, or 'DONE' if there are no more items: ") # this is a string
 
     if selected_id == "DONE":
         break
     else:
-        matching_products = [p for p in products if str(p["id"])  == str(selected_id)]
-        matching_product = matching_products[0]
-        total_price = total_price + matching_product["price"]
-        print("SELECTED PRODUCT: " + matching_product["name"] + " " + str(matching_product["price"]))
-
+        #matching_products = [p for p in products if str(p["id"])  == str(selected_id)]
+        #matching_product = matching_products[0]
+        #total_price = total_price + matching_product["price"]
+        #print("SELECTED PRODUCT: " + matching_product["name"] + " " + str(matching_product["price"]))
+        selected_ids.append(selected_id)
 #
 # INFO DISPLAY (OUTPUT)
 #
- 
+
+for selected_id in selected_ids:
+    matching_products = [p for p in products if str(p["id"])  == str(selected_id)]
+    matching_product = matching_products[0]
+    total_price = total_price + matching_product["price"]
+    print("SELECTED PRODUCT: " + matching_product["name"] + " " + str(matching_product["price"]))
+
 print("TOTAL PRICE: " + str(total_price)) # format into USD
 
 
