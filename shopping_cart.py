666316d6906c6eda783116547f281a787b52ae08
sma299 <59669463+sma299@users.noreply.github.com>
2020-02-02 22:06:04 -0500

    calculations and formatting into dollars

--- shopping_cart.py
+++ shopping_cart.py
@@ -34,6 +34,9 @@ products = [
 # INFO CAPTURE (INPUT)
 #
 total_price = 0
+sales_tax = 0
+tax_percentage = .0875
+real_total = 0
 selected_ids = [] #create a list of ids selected
 
 while True:
@@ -56,16 +59,31 @@ for selected_id in selected_ids:
     matching_products = [p for p in products if str(p["id"])  == str(selected_id)]
     matching_product = matching_products[0]
     total_price = total_price + matching_product["price"]
-    print("SELECTED PRODUCT: " + matching_product["name"] + " " + str(matching_product["price"]))
-
-print("TOTAL PRICE: " + str(total_price)) # format into USD
+    #print("SELECTED PRODUCT: " + matching_product["name"] + " " + str(matching_product["price"]))
 
 
-#A grocery store name of your choice
-#A grocery store phone number and/or website URL and/or address of choice
-#The date and time of the beginning of the checkout process, formatted in a human-friendly way (e.g. 2019-06-06 11:31 AM)
-#The name and price of each shopping cart item, price being formatted as US dollars and cents (e.g. $1.50)
-#The total cost of all shopping cart items, formatted as US dollars and cents (e.g. $4.50), calculated as the sum of their prices
-#The amount of tax owed (e.g. $0.39), calculated by multiplying the total cost by a New York City sales tax rate of 8.75% (for the purposes of this project, groceries are not exempt from sales tax)
-#The total amount owed, formatted as US dollars and cents (e.g. $4.89), calculated by adding together the amount of tax owed plus the total cost of all shopping cart items
-#A friendly message thanking the customer and/or encouraging the customer to shop again
+
+
+#starting the receipt
+
+print("~~~~~~~SAMAR'S STUFF~~~~~~~~~")
+print("phone number: 856-333-7979")
+print("www.samarstuff.com")
+
+#do my calculations
+sales_tax = total_price * tax_percentage
+real_total = total_price + sales_tax
+
+#now I am printing the total
+total_price = "${0:.2f}".format(total_price)
+print("TOTAL PRICE: " + total_price) 
+sales_tax = "${0:.2f}".format(sales_tax)
+print("SALES TAX: " + sales_tax)
+real_total = "${0:.2f}".format(real_total)
+print("TOTAL OWED: " + real_total)
+
+
+#now for a friendly message
+
+print("THANK YOU FOR SHOPPING AT SAMAR'S STUFF!")
+print("HAVE A GREAT DAY")
